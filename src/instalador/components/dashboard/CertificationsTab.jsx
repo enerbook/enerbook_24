f22812082e5507711545e68f680b907a12ad7ad8
victor varac <victor@varac.io>
2025-10-20 15:33:13 -0600

    -

--- src/instalador/components/dashboard/CertificationsTab.jsx
+++ src/instalador/components/dashboard/CertificationsTab.jsx
@@ -13,7 +13,7 @@ const CertificationsTab = ({ certificationStatus, handleCertificationAction }) =
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
             <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="white"/>
           </svg>
-          Agregar Certificación
+          Agrega Certificación
         </button>
       </div>
 
